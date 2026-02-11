149ae8ea3ece0591fad5e9e1801326a3d38edb44
snehasivaram07 <snehasivaram246@gmail.com>
2026-02-11 19:18:23 +0530

    embeddings.py

--- backend/embeddings.py
+++ backend/embeddings.py
@@ -1,16 +1,3 @@
-# from sentence_transformers import SentenceTransformer
-
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-# device="cpu"
-
-# from sentence_transformers import SentenceTransformer
-
-# model = SentenceTransformer("all-MiniLM-L6-v2")
-
-# def embed_texts(chunks):
-#     return model.encode(chunks, show_progress_bar=True)
-
-# 
 
 from sentence_transformers import SentenceTransformer
 import numpy as np
